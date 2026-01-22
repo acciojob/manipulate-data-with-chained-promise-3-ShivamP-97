0864aec6f105ecb979d4b5b5dc5290838a418ab5
ShivamP-97 <shivampathak19972@gmail.com>
2026-01-22 21:04:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function manipulateData() {
+  const inputArray = [1, 2, 3, 4];
+
+  return Promise.resolve(inputArray)
+
+    .then(numbers => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const evenNumbers = numbers.filter(num => num % 2 === 0);
+          document.getElementById("output").textContent = evenNumbers.join(",");
+          resolve(evenNumbers);
+        }, 1000);
+      });
+    })
+
+    .then(evenNumbers => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const multipliedNumbers = evenNumbers.map(num => num * 2);
+          document.getElementById("output").textContent = multipliedNumbers.join(",");
+          resolve(multipliedNumbers);
+        }, 2000);
+      });
+    });
+}
+
+manipulateData();
